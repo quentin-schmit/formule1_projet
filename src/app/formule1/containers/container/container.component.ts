3ad2a7b732539ac8bfcd6486bfb3c76a250229a3
quentin-schmit <schmitquen@eisti.eu>
2022-10-20 23:37:09 +0200

    commit 1

--- src/app/formule1/containers/container/container.component.ts
+++ src/app/formule1/containers/container/container.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
-
 import { flattenDeep } from 'lodash';
 
 import { Drivers } from '../../interfaces/drivers.interface';
@@ -21,18 +20,45 @@ export class ContainerComponent implements OnInit {
 
   Drivers$: Observable<Drivers[]>;
 
+  nationality$: Observable<any[]>;
+
+  filteredNationality$: Observable<Drivers[]>;
+
+  search$ = new BehaviorSubject<string | null>(null);
+
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
     this.res$ = this.httpClient.get<Result>(
-      'https://ergast.com/api/f1/drivers.json'
+      'https://ergast.com/api/f1/drivers.json?limit=1000'
     );
 
     this.Drivers$ = this.res$.pipe(
       map((res) => {
-        console.log(res);
         return res.MRData['DriverTable']['Drivers'];
       })
     );
+
+    this.nationality$ = this.Drivers$.pipe(
+      map((driver) => {
+        let arrayOfnationalty = driver.map((p) => p.nationality);
+
+        return arrayOfnationalty;
+      })
+    );
+
+    this.filteredNationality$ = combineLatest([
+      this.Drivers$,
+      this.search$,
+    ]).pipe(
+      map(([planets, search]) => {
+        if (!search?.length) {
+          return planets;
+        }
+        return planets.filter((p) => p.nationality.includes(search));
+      })
+    );
   }
+
+  public Affiche_ALL_Information() {}
 }
